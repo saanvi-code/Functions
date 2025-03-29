eb8779c1ed5220dc269b3d8330a81627398eb78d
saanvi.code <sa.manni.g9@gmail.com>
2025-03-29 18:41:47 +0530

    first commit

---
+++ p1.py
@@ -0,0 +1,50 @@
+def x():
+    n=int(input("enter the number: "))
+    for i in range(1,11):
+        print(n,"x",i,"=",i*n)
+
+x()
+x()
+
+
+def y():
+    s="autumn"
+    b="winter"
+    print("the weather is pleasant in",s)
+    print("the weather is same in ",b)
+
+y()
+
+def well_wish():
+    print("hello \nhow are you")
+
+well_wish()
+
+def add(n,n2):
+    print(n+n2)
+
+add(1,2)
+
+def substract(n3,n1):
+    print(n3-n1)
+
+def multiply(n4,n5):
+    print(n4*n5)
+
+def power_of_rised_to(n7,n6):
+    print(n7**n6)
+
+substract(2,1)
+multiply(2,1)
+power_of_rised_to(2,1)
+
+def perimeter_of_square(a):
+    print("the perimeter of a square is: ",4*a)
+
+def area_of_a_square(b):
+    print("the area of a square is:",b*b)
+
+perimeter_of_square(2)
+area_of_a_square(2)
+
+
